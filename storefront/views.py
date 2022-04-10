e9a91b9c0e60eb5beab8942b50ec0421e7f3313d
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-10 15:32:53 +0430

    added view sets for product and collection

--- storefront/views.py
+++ storefront/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Product, Collection
+from .serializers import ShowProductSerializer, CreateProductSerializer, CollectionSerializer
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ShowProductSerializer
+        return CreateProductSerializer
