43aabeb23a918c34ec392260795985b9acd4884b
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-10 15:33:12 +0430

    registered routes for collection and product

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework import routers
 
+from . import views
+
 router = routers.DefaultRouter()
+router.register('collections', views.CollectionViewSet, basename='collections')
+router.register('products', views.ProductViewSet, basename='products')
 
 urlpatterns = router.urls
