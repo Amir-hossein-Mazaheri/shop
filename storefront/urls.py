f374f6e21396a6c4c05498821f59ae382b3047a2
LightSpeedGuy <uplayggboy@gmail.com>
2022-04-10 22:25:34 +0430

    added routes for cart and orders and nested route for cart items

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from . import views
 
 router = routers.DefaultRouter()
 router.register('collections', views.CollectionViewSet, basename='collections')
 router.register('products', views.ProductViewSet, basename='products')
+router.register('carts', views.CartViewSet, basename='carts')
+router.register('orders', views.OrderViewSet, basename='orders')
 
-urlpatterns = router.urls
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', views.CartItemViewSet, basename='cart-items')
+
+urlpatterns = router.urls + cart_router.urls
